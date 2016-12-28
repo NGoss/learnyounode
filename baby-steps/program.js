afcd93c22d09582602ff9349fa31a8f12b97adbd
NGoss <ngoss@actionet.com>
2016-12-28 13:37:02 -0500

    Formatting

--- baby-steps/program.js
+++ baby-steps/program.js
@@ -2,11 +2,12 @@ process.argv.shift()
 process.argv.shift()
 
 console.log(
-	process.argv.map(function(string)
-					{
-						return Number(string)
-					})
-				.reduce(function(prev,next)
-					{
-						return prev + next
-					}, 0))
+  process.argv.map(function(string)
+    {
+      return Number(string)
+    })
+  .reduce(function(prev,next)
+  {
+    return prev + next
+  }, 0)
+)
